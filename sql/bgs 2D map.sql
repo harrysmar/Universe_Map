b0958a5692e25e710aed438f209b105ff4806e8a
ken <limstar9147@gmail.com>
2025-09-08 00:59:08 +0900

    Update bgs map.sql

--- sql/bgs 2D map.sql
+++ sql/bgs 2D map.sql
@@ -1,9 +1,30 @@
 SELECT TOP 200000
-  p.ra,
-  p.dec
+  p.ra, p.dec
 FROM
   desi_dr1.target AS t
 JOIN
   desi_dr1.photometry AS p ON t.targetid = p.targetid
 WHERE
   t.bgs_target > 0
+
+SELECT TOP 200000
+  mean_fiber_ra, mean_fiber_dec, z
+FROM 
+  desi_dr1.zpix AS z
+WHERE
+  z.bgs_target > 0
+  
+SELECT TOP 200000
+  mean_fiber_ra, mean_fiber_dec, z
+FROM 
+  desi_dr1.zpix AS z
+WHERE
+  z.bgs_target > 0
+ORDER BY RANDOM() 
+  
+SELECT TOP 200000
+  mean_fiber_ra, mean_fiber_dec, z
+FROM 
+  desi_dr1.zpix AS z
+WHERE
+  z.bgs_target > 0 AND RANDOM() < 0.01
